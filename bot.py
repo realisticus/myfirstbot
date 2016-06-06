bf8373f5189cd6a83d43590116e2017cf3449fe7
realisticus <msavostyanov@gmail.com>
2016-06-07 02:33:30 +0300

    убрал цитирование

--- bot.py
+++ bot.py
@@ -6,11 +6,11 @@ bot = telebot.TeleBot(config.token)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Соня, привет)")
+    bot.send_message(message.chat.id, "Соня, привет!\nБот понимает команды /start, /help и пока что просто отвечает копией присланного ему сообщения")
 
 @bot.message_handler(commands=['help'])
 def send_welcome(message):
-    bot.reply_to(message, "Бот пока что просто отвечает копией присланного ему сообщения")
+    bot.send_message(message.chat.id, "Тут пока ничего нет")
 
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
