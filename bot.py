d9570fcd17a419b9e97202ec921595444728a31f
realisticus <msavostyanov@gmail.com>
2016-06-08 00:48:27 +0300

    small changes

--- bot.py
+++ bot.py
@@ -9,11 +9,11 @@ def send_welcome(message):
     bot.send_message(message.chat.id, "Соня, привет!\nБот понимает команды /start, /help и пока что просто отвечает копией присланного ему сообщения")
 
 @bot.message_handler(commands=['help'])
-def send_welcome(message):
-    bot.send_message(message.chat.id, "Тут пока ничего нет")
+def send_help(message):
+    bot.send_message(message.chat.id, "Тут пока ничего нет, пыщь пыщь")
 
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+def echo_message(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
