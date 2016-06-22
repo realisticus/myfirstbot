b20b43ce6827c9a99a26a0be60c93c2285f942e0
realisticus <msavostyanov@gmail.com>
2016-06-22 11:28:58 +0300

    /start, /help changes

--- bot.py
+++ bot.py
@@ -9,10 +9,10 @@ def shm_word(word):
     word = word.lower()
     vowels = ['а','о','у','ы','е','э','я','и','ю', 'ё']
     try:
-    	first_vowel = min(word.find(vowel) for vowel in vowels if vowel in word)
-    	word = 'шм' + word[first_vowel:]
+        first_vowel = min(word.find(vowel) for vowel in vowels if vowel in word)
+        word = 'шм' + word[first_vowel:]
     except ValueError:
-    	word = 'шм' + word[2:]
+        word = 'шм' + word[2:]
 
     if is_first_capital:
         word = word.title()
@@ -23,11 +23,11 @@ def shm_text(text):
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_message(message.chat.id, "Соня, привет!\nБот понимает команды /start, /help и пока что просто отвечает копией присланного ему сообщения")
+    bot.send_message(message.chat.id, "Привет!\nБот понимает команды /start, /help и пока что просто проказничает с вашими сообщениями")
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
-    bot.send_message(message.chat.id, "Тут пока ничего нет, пыщь пыщь")
+    bot.send_message(message.chat.id, "Тут ничего нет, пыщь пыщь")
 
 @bot.message_handler(content_types=["text"])
 def sh_message(message): # Название функции не играет никакой роли, в принципе
