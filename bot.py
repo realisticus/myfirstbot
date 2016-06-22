f6846c179fcf5e00faff50a8c5e0a0c485c0afbe
realisticus <msavostyanov@gmail.com>
2016-06-22 03:24:26 +0300

    Добавил шм-редупликацию
    sh_word, sh_text

--- bot.py
+++ bot.py
@@ -4,6 +4,23 @@ import telebot
 
 bot = telebot.TeleBot(config.token)
 
+def sh_word(word):
+    is_first_capital = word[0].isupper()
+    word = word.lower()
+    vowels = ['а','о','у','ы','е','э','я','и','ю', 'ё']
+    try:
+    	first_vowel = min(word.find(vowel) for vowel in vowels if vowel in word)
+    	word = 'шм' + word[first_vowel:]
+    except ValueError:
+    	word = 'шм' + word[2:]
+
+    if is_first_capital:
+        word = word.title()
+    return word
+
+def sh_text(text):
+    return ' '.join(filter(None, map(sh_word, text.split())))
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.send_message(message.chat.id, "Соня, привет!\nБот понимает команды /start, /help и пока что просто отвечает копией присланного ему сообщения")
@@ -13,8 +30,8 @@ def send_help(message):
     bot.send_message(message.chat.id, "Тут пока ничего нет, пыщь пыщь")
 
 @bot.message_handler(content_types=["text"])
-def echo_message(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+def sh_message(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, sh_text(message.text))
 
 if __name__ == '__main__':
     bot.polling(none_stop=True)
